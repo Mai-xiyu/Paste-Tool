aeb66c756c4392bc09c86a77239074ea22b78c0e
mai-xiyu <25463800336@qq.com>
2026-03-09 16:05:01 +0800

    Bump version to 0.1.1

--- app_metadata.h
+++ app_metadata.h
@@ -2,7 +2,7 @@
 #define APP_METADATA_H
 
 #define APP_NAME L"Paste Tool"
-#define APP_VERSION L"0.1.0"
+#define APP_VERSION L"0.1.1"
 #define APP_REPOSITORY_URL L"https://github.com/Mai-xiyu/Paste-Tool"
 #define APP_RELEASES_URL L"https://github.com/Mai-xiyu/Paste-Tool/releases"
 #define APP_LATEST_RELEASE_URL L"https://github.com/Mai-xiyu/Paste-Tool/releases/latest"
